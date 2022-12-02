d2d02eae55a252707c572fa831d9063814b14035
merryt <tymerry@gmail.com>
2022-12-01 21:36:20 -0500

    second half of day 1

--- day1.py
+++ day1.py
@@ -1,10 +1,16 @@
 
 day1input = open("./day1input.txt", "r" )
-# output = day1input.read()
-# elves_load = output.split('\n\n')
-# elves_load_calories = [ sum([int(i) for i in x.split('\n') if i !='']) for x in elves_load]
-# print(sorted(elves_load_calories)[-1])
+output = day1input.read()
+elves_load = output.split('\n\n')
+elves_load_calories = [ sum([int(i) for i in x.split('\n') if i !='']) for x in elves_load]
+sorted_loaded_elves = sorted(elves_load_calories)
+
+print("----Part 1-----")
+print(sorted_loaded_elves[-1])
+
+print("----Part 2-----")
+print(sum(sorted_loaded_elves[-3:]))
 
 
-output = print(sorted([ sum([int(i) for i in x.split('\n') if i !='']) for x in day1input.read().split('\n\n')])[-1])
+
 day1input.close()
