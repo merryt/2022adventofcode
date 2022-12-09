76b9cbd0ac059e1fa6dc34305a95aa5b030fc489
merryt <tymerry@gmail.com>
2022-12-09 01:12:33 -0600

    day 9 all

--- day9.py
+++ day9.py
@@ -1,30 +1,29 @@
 import copy
 
+def move_h(direction):
+    if direction == "L":
+        h_position["x"] -= 1
+    if direction == "R":
+        h_position["x"] += 1
+    if direction == "U":
+        h_position["y"] += 1
+    if direction == "D":
+        h_position["y"] -= 1
 
 def move_knot(following, current):
-    """
-    TODO: work on tracking the tail 
-    """
-    
     # same horizontal position
     if following["x"] == current["x"]:
         if (abs(following["y"] - current["y"]) > 1) and (following["y"] > current["y"]):
             current["y"] += 1
         if (abs(following["y"] - current["y"]) > 1) and (following["y"] < current["y"]):
             current["y"] -= 1
-        return current
-        # t_history.append(copy.deepcopy(current))
-        
-            
-            
+                        
     # same virtical position 
     if following["y"] == current["y"]:
         if (abs(following["x"] - current["x"]) > 1) and (following["x"] > current["x"]):
             current["x"] += 1
         if (abs(following["x"] - current["x"]) > 1) and (following["x"] < current["x"]):
             current["x"] -= 1
-        return current
-        # t_history.append(copy.deepcopy(current))
 
     #diagonally okay
     if (abs(following["x"] - current["x"]) <= 1) and (abs(following["y"] - current["y"]) <= 1):
@@ -43,30 +42,8 @@ def move_knot(following, current):
         current["y"] += 1
     
     return current
-    # t_history.append(copy.deepcopy(current))
     
-    
-    
-    
-    
-    
-    
-    
-
-def move_h(direction):
-    if direction == "L":
-        h_position["x"] -= 1
-    if direction == "R":
-        h_position["x"] += 1
-    if direction == "U":
-        h_position["y"] += 1
-    if direction == "D":
-        h_position["y"] -= 1
-
-"""
-hotplate
-"""
-day9input = open("./day9small.txt", "r" )
+day9input = open("./day9input.txt", "r" )
 output = day9input.read()
 day9input.close()
 instructions = [ [ x.split(" ")[0], int(x.split(" ")[1])] for x in output.split('\n') if x != ""]
@@ -75,35 +52,25 @@ h_position = {"x":0, "y":0}
 
 t_position = {"x":0,"y":0,}
 t_history = []
-b_pos =  [{"x":0,"y":0,},{"x":0,"y":0,},{"x":0,"y":0,},{"x":0,"y":0,},{"x":0,"y":0,},{"x":0,"y":0,},{"x":0,"y":0,},{"x":0,"y":0,}]
+b_pos = [{"x":0,"y":0,},{"x":0,"y":0,},{"x":0,"y":0,},{"x":0,"y":0,},{"x":0,"y":0,},{"x":0,"y":0,},{"x":0,"y":0,},{"x":0,"y":0,}]
 
 
 for x in instructions:
     for step in range(x[1]):
         move_h(x[0])
-
-        t_history.append(copy.deepcopy(move_knot(h_position,t_position)))
+        move_knot(h_position, b_pos[0])
+        for i in range(1,8):
+            move_knot(b_pos[i-1],b_pos[i])
         
-        # print("----post head & tail move----")
-        # print(h_position)
-        # print(t_position)
-        # print("--------")
+        t_history.append(copy.deepcopy(move_knot(b_pos[7],t_position)))
 
 
 unique_locations = []
 for location in t_history:
     if location not in unique_locations:   
-        unique_locations.append(location)
-        
+        unique_locations.append(location)      
 
-
-
-
-
-print("----p1------")
+print("----p1 & 2------")
 print(len(unique_locations))
-
-print("----p2------")
-# print(bestview)
 
 
